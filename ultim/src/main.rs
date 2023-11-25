23a78819be259893474ac2fccc2a9b359648b838
sin-ack <sin-ack@users.noreply.github.com>
2023-11-25 17:55:21 +0000

    Setup a basic web server based on actix-web

--- ultim/src/main.rs
+++ ultim/src/main.rs
@@ -1,3 +1,13 @@
-pub fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    "Hello world!"
+}
+
+#[actix_web::main]
+pub async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind(("0.0.0.0", 8000))?
+        .run()
+        .await
 }
